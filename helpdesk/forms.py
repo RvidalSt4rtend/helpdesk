79266ff4cbd141a2066d4d2ffc53aaa80e7aebf0
Ardal <rvidal@st4rtend.com>
2025-06-14 13:14:06 -0500

    udapte estlois

--- helpdesk/forms.py
+++ helpdesk/forms.py
@@ -9,11 +9,57 @@ class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = ['title', 'description', 'prioridad', 'adjunto', 'tipo']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'bg-dark-secondary border-dark-tertiary text-white',
+                'style': 'background-color:#232336 !important;color:#fff !important;border-color:#2d2d44 !important;'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'bg-dark-secondary border-dark-tertiary text-white',
+                'rows': 6,
+                'style': 'background-color:#232336 !important;color:#fff !important;border-color:#2d2d44 !important;'
+            }),
+            'prioridad': forms.Select(attrs={
+                'class': 'bg-dark-secondary border-dark-tertiary text-white',
+                'style': 'background-color:#232336 !important;color:#fff !important;border-color:#2d2d44 !important;'
+            }),
+            'tipo': forms.Select(attrs={
+                'class': 'bg-dark-secondary border-dark-tertiary text-white',
+                'style': 'background-color:#232336 !important;color:#fff !important;border-color:#2d2d44 !important;'
+            }),
+            'adjunto': forms.ClearableFileInput(attrs={
+                'class': 'bg-dark-secondary border-dark-tertiary text-white',
+                'style': 'background-color:#232336 !important;color:#fff !important;border-color:#2d2d44 !important;'
+            }),
+        }
 
 class TicketUpdateForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = ['title', 'description', 'prioridad', 'estado', 'adjunto']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'bg-dark-secondary border-dark-tertiary text-white',
+                'style': 'background-color:#232336 !important;color:#fff !important;border-color:#2d2d44 !important;'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'bg-dark-secondary border-dark-tertiary text-white',
+                'rows': 6,
+                'style': 'background-color:#232336 !important;color:#fff !important;border-color:#2d2d44 !important;'
+            }),
+            'prioridad': forms.Select(attrs={
+                'class': 'bg-dark-secondary border-dark-tertiary text-white',
+                'style': 'background-color:#232336 !important;color:#fff !important;border-color:#2d2d44 !important;'
+            }),
+            'estado': forms.Select(attrs={
+                'class': 'bg-dark-secondary border-dark-tertiary text-white',
+                'style': 'background-color:#232336 !important;color:#fff !important;border-color:#2d2d44 !important;'
+            }),
+            'adjunto': forms.ClearableFileInput(attrs={
+                'class': 'bg-dark-secondary border-dark-tertiary text-white',
+                'style': 'background-color:#232336 !important;color:#fff !important;border-color:#2d2d44 !important;'
+            }),
+        }
 
 class ClienteTicketUpdateForm(forms.ModelForm):
     """
@@ -28,3 +74,9 @@ class InteraccionTicketForm(forms.ModelForm):
     class Meta:
         model = InteraccionTicket
         fields = ['mensaje']
+        widgets = {
+            'mensaje': forms.Textarea(attrs={
+                'class': 'bg-dark-secondary border-dark-tertiary text-secondary-500',
+                'rows': 4
+            })
+        }
